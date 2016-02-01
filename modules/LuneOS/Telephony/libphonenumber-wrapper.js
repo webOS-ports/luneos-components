0d0aa774d5af6fdcd4f7d7c732b46246a73ff605
Christophe Chapuis <chris.chapuis@gmail.com>
2016-02-01 14:24:35 +0100

    Telephony: add telephony-lib from luneos as the primary solution
    The phonenumber JS library from google weights 400Kb, and
    introduces a big overhead (5-10s) to the phone app loading time.
    
    
    Signed-off-by: Christophe Chapuis <chris.chapuis@gmail.com>

--- modules/LuneOS/Telephony/libphonenumber-wrapper.js
+++ modules/LuneOS/Telephony/libphonenumber-wrapper.js
@@ -1,9 +1,12 @@
 .pragma library
 
+// Only some simple wrappers around PhoneNumber.js to easily use it from QML
+
+/**** version using google-phonenumber JS library ****/
+/*
 var modules = modules || {};
 Qt.include("libphonenumber.js");
 
-// Only some simple wrappers around libphonenumber.js to easily use it from QML
 var PhoneNumberFormat = i18n.phonenumbers.PhoneNumberFormat;
 var PhoneNumberUtil = i18n.phonenumbers.PhoneNumberUtil.getInstance();
 
@@ -16,6 +19,27 @@ function normalizePhoneNumber(phoneNumber, countryCode)
     // reverse of "+-countrycode--national_number-extension"
 
     var normalizedNumber = "+-"+phoneNumberObj.getCountryCodeOrDefault() + "--" + phoneNumberObj.getNationalNumberOrDefault() + "-" + phoneNumberObj.getExtensionOrDefault();
+    var reversedNormalizedNumber = normalizedNumber.split("").reverse().join("");
+
+    console.log("normalizePhoneNumber("+phoneNumber+")="+reversedNormalizedNumber);
+    return reversedNormalizedNumber;
+}
+*/
+
+/**** version using luneos' telephony-lib ******/
+Qt.include("phonenumber.js/PhoneNumberMetadata.js");
+Qt.include("phonenumber.js/PhoneNumberNormalizer.js");
+Qt.include("phonenumber.js/PhoneNumber.js");
+
+function normalizePhoneNumber(phoneNumber, countryCode)
+{
+    var phoneNumberObj = PhoneNumber.Parse(phoneNumber, countryCode);
+
+    // try to be compatible with Enyo g11n
+
+    // reverse of "+-countrycode--national_number-extension"
+
+    var normalizedNumber = "+-"+phoneNumberObj.regionMetaData.countryCode + "--" + phoneNumberObj.nationalNumber + "-";
     var reversedNormalizedNumber = normalizedNumber.split("").reverse().join("");
 
     console.log("normalizePhoneNumber("+phoneNumber+")="+reversedNormalizedNumber);
